4b66e20f459a56166afc11b0b82cf727d846372c
mddeepcrawl <mariya.danilova@deepcrawl.com>
2019-12-17 11:17:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const debug = require("debug")("app:startup");
+const config = require("config");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const Joi = require("joi");
+const logger = require("./middleware/logger");
+const courses = require("./routes/courses");
+const courses = require("./routes/home");
+const express = require("express");
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+app.use("/api/courses", courses);
+app.use("/", home);
+
+console.log("Application Name: " + config.get("name"));
+console.log("Mail server: " + config.get("mail.host"));
+console.log("Password server: " + config.get("mail.password"));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  debug("Morgan enabled...");
+}
+
+app.use(logger);
+
+app.use(function(req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
